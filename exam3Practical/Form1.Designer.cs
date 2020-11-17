3a41abedb1e0527ef3566d6b79e8366fe2c6c845
Drake Hetland <hetlandd2289@ILCC.CC.IA.US>
2020-11-17 10:45:59 -0600

    exam 3

---
+++ exam3Practical/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace exam3Practical
+{
+    partial class exam3Practical
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.problemLabel = new System.Windows.Forms.Label();
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(67, 38);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(344, 13);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Enter the answer to the addition problem in the text box provided below.";
+            // 
+            // problemLabel
+            // 
+            this.problemLabel.AutoSize = true;
+            this.problemLabel.Location = new System.Drawing.Point(67, 70);
+            this.problemLabel.Name = "problemLabel";
+            this.problemLabel.Size = new System.Drawing.Size(0, 13);
+            this.problemLabel.TabIndex = 1;
+            // 
+            // answerTextBox
+            // 
+            this.answerTextBox.Location = new System.Drawing.Point(70, 111);
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.answerTextBox.TabIndex = 2;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(70, 153);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(70, 215);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(70, 186);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(75, 23);
+            this.displayButton.TabIndex = 5;
+            this.displayButton.Text = "Display Incorrect Answers";
+            this.displayButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(177, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // exam3Practical
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.answerTextBox);
+            this.Controls.Add(this.problemLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "exam3Practical";
+            this.Text = "Exam 3 Practical";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label problemLabel;
+        private System.Windows.Forms.TextBox answerTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
