54a166409ffddce68c445c39c9ee5a421c67a3f9
hetlandd2289 <73547873+hetlandd2289@users.noreply.github.com>
2021-08-27 10:01:00 -0500

    C++ in class example

---
+++ C++/Example/Example.cpp
@@ -0,0 +1,18 @@
+// Auth: Drake Hetland
+// Date: 8/27/2021
+// Desc: Simple in class C++ Chapeter 2 Example
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "How are you doing?" << endl << "Hopefully you are well! \r \\";
+    cout << "\"Weather is poor here\" \a ";
+
+    return 0;
+}
+
+
+
