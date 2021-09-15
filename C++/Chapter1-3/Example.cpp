c0f110d0c0f51260b3fd68c2ee924799a284ccaf
hetlandd2289 <73547873+hetlandd2289@users.noreply.github.com>
2021-09-15 09:57:00 -0500

    in class example work

--- C++/Chapter1-3/Example.cpp
+++ C++/Chapter1-3/Example.cpp
@@ -3,6 +3,11 @@
 // Desc: Simple in class C++ Chapeter 2 Example
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
@@ -12,6 +17,35 @@ int main()
     cout << "\"Weather is poor here\" \a ";
 
     return 0;
+
+    
+}
+
+int getCoverage() {
+    float cov = 0;
+
+    cout << "Enter the square feet that each gallon can cover: ";
+    cin >> cov;
+
+    if (cov > 0) {
+        return cov;
+    }
+
+    else {
+        return 340;
+    }
+}
+
+void howMuchPaint() {
+    float height = 0.0, length = 0.0;
+    float coverage = getCoverage();
+
+    cout << "Height of your fence in feet: ";
+    cin >> height;
+    cout << "Length of your fence in feet: ";
+    cin >> length;
+
+    cout << "You will need " << setprecision(3) << (length * height * 2 / coverage) << " gallons of paint for two coats of paint.\n";
 }
 
 
