60f8bd999de0e16b4e70962c4004f98dbb97632c
hetlandd2289 <73547873+hetlandd2289@users.noreply.github.com>
2021-09-29 09:51:18 -0500

    in class restaurant selector example

--- C++/Chapter1-3/Example.cpp
+++ C++/Chapter1-3/Example.cpp
@@ -11,12 +11,9 @@
 using namespace std;
 
 void resturantSelector() {
-    //create enumerated data type
-    enum yn{y, n};
-
+    
     // Delcare variables
     string ve = "", va = "", gf = "";
-    yn vege = n, vega = n, glut = n;
 
     cout << "Is anyone in your party a vegetarian? [y/n]: ";
     cin >> ve;
@@ -24,23 +21,52 @@ void resturantSelector() {
     cin >> va;
     cout << "Is anyone in your party gluten-free [y/n]: ";
     cin >> gf;
+
+    
+    if (ve == "n" && va == "n" && gf == "n") {
+        // Joe's Gorumet Burgers
+        // Mama's Fine Italian
+        // Main Street Pizza
+        // The Chefs Kitchen
+        // Corner Cafe
+        cout << "Here are your resturant choices:\n-Joe's Gorumet Burgers\n-Mama's Fine Italian\n-Main Street Pizza\n-The Chefs Kitchen\n-Corner Cafe\n";
+    }
+    else if (ve == "y" && va == "n" && gf == "n") {
+        // Mama's Fine Italian
+        // Main Street Pizza
+        // The Chefs Kitchen
+        // Corner Cafe
+        cout << "Here are your resturant choices:\n-Mama's Fine Italian\n-Main Street Pizza\n-The Chefs Kitchen\n-Corner Cafe\n";
+    }
+    else if (va == "n" && gf == "y") {
+        // Main Street Pizza
+        // The Chefs Kitchen
+        // Corner Cafe
+        cout << "Here are your resturant choices:\n-Main Street Pizza\n-The Chefs Kitchen\n-Corner Cafe\n";
+    }
+    else {
+        // The Chefs Kitchen
+        // Corner Cafe
+        cout << "Here are your resturant choices:\n-The Chefs Kitchen\n-Corner Cafe\n";
+    }
 }
 
 int main()
 {
     int choice = -1;
+    string str;
 
     while (choice != 0) {
         cout << "1 - Tell Joke\n"
              << "2 - Punchline\n"
-             << "7 - Resturant Selector \n"
-             << "0 - Quit";
+             << "7 - Resturant Selector\n"
+             << "0 - Quit\n";
         cin >> choice;
 
         switch (choice) {
-            case 1:
+        case 1: cout << "Haha just kidding no joke";
                 break;
-            case 2:
+        case 2: cout << "Bruh I said there was no joke";
                 break;
             case 3: 
                 break;
