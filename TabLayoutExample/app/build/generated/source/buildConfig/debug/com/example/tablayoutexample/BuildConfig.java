d9f12a7af0c28d9ff61138f054233afcbc304475
hetlandd2289 <73547873+hetlandd2289@users.noreply.github.com>
2022-04-05 12:03:15 -0500

    yeahg

---
+++ TabLayoutExample/app/build/generated/source/buildConfig/debug/com/example/tablayoutexample/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.tablayoutexample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.tablayoutexample";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
